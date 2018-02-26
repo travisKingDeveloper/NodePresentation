e872d3554ee4cd0a9681df20a4f1ac06ad5c4e98
Travis King <travis.king@advent.com>
2018-02-26 14:05:11 -0500

    Added Code that post and get all information from users table in mongo

--- expressPresentation/routes/users.js
+++ expressPresentation/routes/users.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/presentation')
 
-module.exports = router;
+const User = require('../repository/userSchema')
+
+router.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    if (err) return res.status(500).send("There was a problem finding the users")
+
+    res.status(200).send(users)
+  })
+})
+
+router.post('/', function(req, res) {
+  const {
+    name,
+    username,
+    password
+  } = req.body
+
+  const user = new User({
+    name,
+    username,
+    password,
+    location: 'there',
+    admin: false,
+  })
+
+  user.save(function (err) {
+    if(!err) {
+      return res.json({
+        status: 'OK',
+        user,
+      })
+    } else {
+      res.statusCode = 500
+      res.json({
+        error: err
+      })
+    }
+  })
+})
+
+module.exports = router
